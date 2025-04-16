867aa3c471abd2320339b273a0f904447cd9eb39
Danny <kukoladanny@gmail.com>
2025-04-16 11:29:13 -0400

    Modified HelloWorld.java by adding a user's favourite colour and modified the number printed out

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,13 +4,19 @@ public class HelloWorld {
     public static void main(String[] args) {
         // Creat a scanner to read
         Scanner sc = new Scanner(System.in);
+
         System.out.print("Enter your name: ");
         String name = sc.next();
-        System.out.println("Hello "+name);
+
+        System.out.print("Enter your favourite colour: ");
+        String colour = sc.next();
+
+        
+        System.out.println("Hello "+name + ", your favourite colour is " + colour);
         sc.close();   
 
         // Simple shortform example
-        int num = 4;
+        int num = 10;
         System.out.println(num);
         System.out.println(num++);
         System.out.println(++num);
